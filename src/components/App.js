236c045e5041506bb2feffc6b0dacd82157a65f7
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-12-21 07:54:00 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './Header'
+import AddContact from './AddContact';
+import ContactList from './ContactList';
+import ContactCard from './ContactCard';
+
+
+function app() {
+  return (
+    <div>
+      <Header/>
+      {/*<AddContact/>
+      <ContactList/>
+  <ContactCard/>*/}
+
+    </div>
+  )
+}
+
+export default app;
